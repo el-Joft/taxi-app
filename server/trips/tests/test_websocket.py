f7ce4ee3011713b5012ce89d1661ce03f7e1572e
el-joft <ottimothy@gmail.com>
2020-12-30 16:09:57 +0100

    websocket authentication

--- server/trips/tests/test_websocket.py
+++ server/trips/tests/test_websocket.py
@@ -3,6 +3,10 @@ from channels.layers import get_channel_layer
 
 import pytest
 
+from django.contrib.auth import get_user_model
+from channels.db import database_sync_to_async
+from rest_framework_simplejwt.tokens import AccessToken
+
 from taxi.routing import application
 
 TEST_CHANNEL_LAYERS = {
@@ -11,14 +15,30 @@ TEST_CHANNEL_LAYERS = {
   },
 }
 
+
+
+@database_sync_to_async
+def create_user(username, password):
+    user = get_user_model().objects.create_user(
+        username=username,
+        password=password
+    )
+    access = AccessToken.for_user(user)
+    return user, access
+
 @pytest.mark.asyncio
+# we need to add the below to be able to access the database
+@pytest.mark.django_db(transaction=True) # new
 class TestWebSocket:
     async def test_can_connect_to_server(self, settings):
         # making settings to use default django in mermory instead of redis
         settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        # call the created user which returns access token
+        _, access = await create_user('test.user@example.com', 'pAssw0rd')
         communicator = WebsocketCommunicator(
             application=application,
-            path='/taxi/'
+            # path='/taxi/' # without token
+            path=f'/taxi/?token={access}' # with token
         )
         connected, _ = await communicator.connect()
         assert connected is True
@@ -42,4 +62,12 @@ class TestWebSocket:
         assert response == message
         await communicator.disconnect()
 
-    # test using a channel layer to broadcast messgae to a channel group
+    # test connection fail without a valid token
+    async def test_cannot_connect_to_socket(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(
+            application=application,
+            path='/taxi/'
+        )
+        connected, _ = await communicator.connect()
+        assert connected is False
